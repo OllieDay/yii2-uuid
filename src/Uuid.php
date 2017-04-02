bbef447ea443493aae2f9696ece3f656ffaf1b1e
Ollie Day <OllieDay@users.noreply.github.com>
2017-04-02 18:16:50 +0100

    Use tabs

--- src/Uuid.php
+++ src/Uuid.php
@@ -12,49 +12,49 @@ use yii\base\Component;
 */
 class Uuid extends Component
 {
-    /**
-     * @see \Ramsey\Uuid\Uuid::uuid1()
-     *
-     * @param int|string $node
-     * @param int $clockSeq
-     * @return \Ramsey\Uuid\UuidInterface
-     */
-    public function uuid1($node = null, $clockSeq = null)
-    {
-        return \Ramsey\Uuid\Uuid::uuid1($node, $clockSeq);
-    }
+	/**
+	 * @see \Ramsey\Uuid\Uuid::uuid1()
+	 *
+	 * @param int|string $node
+	 * @param int $clockSeq
+	 * @return \Ramsey\Uuid\UuidInterface
+	 */
+	public function uuid1($node = null, $clockSeq = null)
+	{
+		return \Ramsey\Uuid\Uuid::uuid1($node, $clockSeq);
+	}
 
-    /**
-    * @see \Ramsey\Uuid\Uuid::uuid3()
-    *
-    * @param string $ns
-    * @param string $name
-    * @return \Ramsey\Uuid\UuidInterface
-    */
-    public function uuid3($ns, $name)
-    {
-        return \Ramsey\Uuid\Uuid::uuid3($ns, $name);
-    }
+	/**
+	* @see \Ramsey\Uuid\Uuid::uuid3()
+	*
+	* @param string $ns
+	* @param string $name
+	* @return \Ramsey\Uuid\UuidInterface
+	*/
+	public function uuid3($ns, $name)
+	{
+		return \Ramsey\Uuid\Uuid::uuid3($ns, $name);
+	}
 
-    /**
-    * @see \Ramsey\Uuid\Uuid::uuid4()
-    *
-    * @return \Ramsey\Uuid\UuidInterface
-    */
-    public function uuid4()
-    {
-        return \Ramsey\Uuid\Uuid::uuid4();
-    }
+	/**
+	* @see \Ramsey\Uuid\Uuid::uuid4()
+	*
+	* @return \Ramsey\Uuid\UuidInterface
+	*/
+	public function uuid4()
+	{
+		return \Ramsey\Uuid\Uuid::uuid4();
+	}
 
-    /**
-    * @see \Ramsey\Uuid\Uuid::uuid5()
-    *
-    * @param string $ns
-    * @param string $name
-    * @return \Ramsey\Uuid\UuidInterface
-    */
-    public function uuid5($ns, $name)
-    {
-        return \Ramsey\Uuid\Uuid::uuid5($ns, $name);
-    }
+	/**
+	* @see \Ramsey\Uuid\Uuid::uuid5()
+	*
+	* @param string $ns
+	* @param string $name
+	* @return \Ramsey\Uuid\UuidInterface
+	*/
+	public function uuid5($ns, $name)
+	{
+		return \Ramsey\Uuid\Uuid::uuid5($ns, $name);
+	}
 }
